42ec4e089f67d7e60c5411e4d041070d868f1ff0
sinclairnick <nicksinclair@xtra.co.nz>
2019-10-01 22:22:09 +1300

    [fix] revert to original cpu mapping

--- test_audio.py
+++ test_audio.py
@@ -22,7 +22,7 @@ if __name__ == '__main__':
     GENERATOR = opt.generator
 
     generator = Generator()
-    generator.load_state_dict(torch.load(GENERATOR, map_location=torch.device('cpu')))
+    generator.load_state_dict(torch.load(GENERATOR, map_location='cpu'))
     if torch.cuda.is_available():
         generator.cuda()
     generator.eval()
